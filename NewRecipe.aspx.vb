d44b42dab5ce3d2420dae1537c15474c8aa59158
Lazarus Guchu <ngugilaz@gmail.com>
2014-03-24 12:47:21 -0500

    modified aspx files and added initial style.css

--- NewRecipe.aspx.vb
+++ NewRecipe.aspx.vb
@@ -2,4 +2,15 @@
 Partial Class detailsView
     Inherits System.Web.UI.Page
 
+    Protected Sub DetailsView1_ItemInserted(sender As Object, e As DetailsViewInsertedEventArgs) Handles DetailsView1.ItemInserted
+        Response.Redirect("recipe.aspx")
+
+        Dim delRecipe As String = e.Values("reName").ToString()
+        Response.Write("The recipe")
+        Response.Write("<span class=deletedrecipehighlight>")
+        Response.Write(delRecipe)
+        Response.Write("</span> has been deleted.")
+        Response.AddHeader("Refresh", "3:url=recipe.aspx")
+
+    End Sub
 End Class
--- Recipedetails.aspx.vb
+++ Recipedetails.aspx.vb
@@ -2,4 +2,13 @@
 Partial Class detailsView
     Inherits System.Web.UI.Page
 
+    Protected Sub DetailsView1_ItemDeleted(sender As Object, e As DetailsViewDeletedEventArgs) Handles DetailsView1.ItemDeleted
+        Response.Write("The recipe has been deleted")
+    End Sub
+
+    Protected Sub DetailsView1_ItemUpdated(sender As Object, e As DetailsViewUpdatedEventArgs) Handles DetailsView1.ItemUpdated
+        Response.Redirect("recipe.aspx")
+
+    End Sub
+
 End Class
